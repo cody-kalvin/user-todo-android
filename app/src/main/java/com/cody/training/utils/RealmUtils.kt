740d76573e0c14b57b87fa62c2b0fa25c293aaa3
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2020-11-19 09:26:43 +0800

    remove unused resoures

--- app/src/main/java/com/cody/training/utils/RealmUtils.kt
+++ app/src/main/java/com/cody/training/utils/RealmUtils.kt
@@ -2,7 +2,5 @@ package com.cody.training.utils
 
 import com.cody.training.data.todo.TodoRepository
 import io.realm.Realm
-import io.realm.RealmModel
-import io.realm.RealmResults
 
 fun Realm.todoRepository(): TodoRepository = TodoRepository(this)
